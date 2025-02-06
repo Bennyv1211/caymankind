d561295fe06e9e57055ac61b2f129a2655b18756
Benny <benjaminvermeulen22@gmail.com>
2025-02-06 18:12:14 -0500

    Fix Vercel deployment errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from ".//src/App.tsx";  // Ensure this matches the filename (App.tsx)
+import App from './App';
 import reportWebVitals from './reportWebVitals';  // Ensure this file exists
 
 const root = ReactDOM.createRoot(
